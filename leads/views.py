d34d4789086fc248443af79f95794e65701df479
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-07 14:56:12 -0500

    urls

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Lead
 
-# Create your views here.
+
+def index(request):
+    leads = Lead.objects.all()
+    
+    context = {
+        'leads':leads,
+    }
+
+
+    return render(request, 'leads/index.html', context)
+    
