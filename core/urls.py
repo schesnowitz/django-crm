7429261250f66d072cee7e9c3056ce7a5774a257
Stephen Chesnowitz <steve@chesnowitz.com>
2023-02-08 18:19:15 -0500

    update view html

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-from leads.views import landing_page, LandingView
+from leads.views import LandingView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,6 +8,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('leads/', include('leads.urls')),
-    # path('', landing_page, name='landing-page'),
-    path('', LandingView.as_view(), name='landing-page'), 
+    path('', LandingView.as_view(), name='landing-page')
 ]
